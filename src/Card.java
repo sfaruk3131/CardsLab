edd6b62b5ad26a6b7a3af5fee2264c218ca2076b
sfaruk3131 <sfaruk3131@bths.edu>
2019-01-07 22:19:59 -0500

    1

--- src/Card.java
+++ src/Card.java
@@ -1,7 +1,37 @@
 public class Card {
-    private String Ace;
-    private String Hearts;
-    private String Clubs;
-    private String Spades;
+    private String Rank;
+    private String Suit;
+    private int PointValue;
 
+    public Card(String rank, String suit, int pointValue)
+    {
+        this.Rank = Rank;
+        this.Suit = Suit;
+        this.PointValue = PointValue;
+    }
+
+    public String getRank()
+    {
+        return Rank;
+    }
+
+    public String suit()
+    {
+        return Suit;
+    }
+
+    public int PointValue()
+    {
+        return PointValue;
+    }
+
+    public boolean equals(Card otherCard)
+    {
+        return Rank.equals(otherCard) && Suit.equals(otherCard.Suit) && PointValue == otherCard.PointValue;
+    }
+
+    public String toString()
+    {
+        return Rank + " of " + Suit + ": " + PointValue + " point(s)";
+    }
 }
