87fe8ccd91b2cd8b110cb7806576b00d98a38523
BT_1N3_12 <sfaruk3131@bths.edu>
2019-01-10 10:12:40 -0500

    1

---
+++ src/Deck.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Deck {
+    private ArrayList<Card>Dealt;
+    private ArrayList<Card>unDealt;
+    public Deck(String[] Ranks, String[] Suits, int[] Values)
+    {
+        
+    }
+
+
+}
